bf816f7ebe73a021e3fe7a9271730b38f7912aa3
orutanakaito <voltgt777@gmail.com>
2023-12-31 19:56:35 +0900

    paintComponent関数をオーバライドして,プレイヤーと図形を描画しています。

--- src/main/kotlin/main/GamePanel.kt
+++ src/main/kotlin/main/GamePanel.kt
@@ -1,5 +1,15 @@
 package main
 
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Font
+import java.awt.Graphics
+import java.awt.event.KeyEvent
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+import javax.swing.JLabel
 import javax.swing.JPanel
 
 class GamePanel : JPanel() {
@@ -14,8 +24,54 @@ class GamePanel : JPanel() {
 
     private val maxScreenCol = 12
 
-    val screenWidth = tileSize * maxScreenRow
+    private val screenWidth = tileSize * maxScreenRow
 
-    val screenHeight: Int = tileSize * maxScreenCol
+    private val screenHeight: Int = tileSize * maxScreenCol
 
+    private lateinit var drawPlayer: BufferedImage
+
+    private var playerX = 0
+
+    private var playerY = 0
+
+    init {
+
+        this.preferredSize = Dimension(screenWidth, screenHeight)
+
+        setBackground(Color.BLACK)
+
+        val jl = JLabel("RPG")
+
+        jl.setFont(Font(Font.MONOSPACED, Font.BOLD, 20))
+
+        jl.setForeground(Color.WHITE)
+
+        add(jl)
+
+        try {
+
+            val file = File("C:\\Users\\meteo\\IdeaProjects\\Swing_rpg\\res\\player.png")
+
+            drawPlayer = ImageIO.read(file)
+
+            drawPlayer = drawPlayer.getSubimage(0, 0, 32, 32)
+
+        } catch (e: IOException) {
+
+            e.printStackTrace()
+        }
+    }
+
+    override fun paintComponent(g: Graphics) {
+
+        super.paintComponent(g)
+
+        requestFocusInWindow()
+
+        g.color = Color.ORANGE
+
+        g.fillRect(100, 100, 500, 300)
+
+        g.drawImage(drawPlayer, playerX, playerY, null)
+    }
 }
